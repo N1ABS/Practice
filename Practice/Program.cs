5298d0f646850f80c7852ce781dd854fbe9a73af
Nurbolat Absagat <n.absagat@gmail.com>
2024-05-03 15:00:07 +0500

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,50 @@
+﻿using Practice;
+using System;
+
+
+// создание объекта в конструктором по умолчанию
+var pryamougolnik = new Shape();
+
+// setting of values
+pryamougolnik.Length = 10;
+pryamougolnik.Width = 5;
+
+// run Square method
+var ploshad1 = pryamougolnik.Square();
+
+
+// show output
+Console.WriteLine(ploshad1);
+Console.WriteLine(pryamougolnik.Audany);
+
+// создание объекта через перегруженный конструктор
+var kvadrat = new Shape(7, 7);
+
+
+// run Square method
+var ploshad2 = kvadrat.Square();
+
+//show output
+Console.WriteLine(ploshad2);
+Console.WriteLine(kvadrat.Length);
+Console.WriteLine(kvadrat.Width);
+
+
+// Бүгінгі сабақта:
+
+// Classes and Objects
+// Constructors
+// Methods
+// Data types
+
+
+// Келесі сабақта:
+
+// Abstract classes
+// Interfaces
+// Virtual methods
+// Overridded methods
+
+// Наследование
+// Инкапсуляция
+// Полиморфизм
---
+++ Practice/Shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Practice
+{
+    // Класс Shape
+    public class Shape
+    {
+        // Свойства (Properties)
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public double Audany { get; set; }
+
+        // Default constructor (Конструктор по умолчанию / пустой конструктор)
+        public Shape()
+        {
+            
+        }
+
+        // Перегрузка конструктора с двумя параметрами
+        public Shape(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        // Метод для нахождения площади
+        public double Square()
+        {
+            Audany = Length * Width;
+            return Audany;
+        }
+
+        public void SquareConsole()
+        {
+            Audany = Length * Width;
+            Console.WriteLine(Audany);
+        }
+
+    }
+}
