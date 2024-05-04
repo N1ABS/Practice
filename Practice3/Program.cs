d9e3534f55502538ae45daf769ce77dbe6835cd2
Nurbolat Absagat <n.absagat@gmail.com>
2024-05-04 16:37:30 +0500

    Upcasting and downcasting

---
+++ Practice3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Practice3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person anuar = new Person();
+            anuar.Name = "Anuar";
+
+            anuar.Print();
+
+            Employee nurbolat = new Employee();
+            nurbolat.Name = "Nurbolat";
+            nurbolat.Company = "ATI";
+            nurbolat.Print();
+
+            Person nurbolat2 = nurbolat;
+            nurbolat2.Print();
+        }
+    }
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public virtual void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string Company { get; set; }
+
+        public override void Print()
+        {
+            Console.WriteLine(Name + " works at " + Company);
+        }
+    }
+}
